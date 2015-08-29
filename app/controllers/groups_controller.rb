3064a8e72e7181d760a3d6c373e801825cedcc6f
ryan.liu <moomoo9513@gmail.com>
2015-08-29 21:04:42 +0800

    Complete groups#create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,11 +18,23 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.create(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
