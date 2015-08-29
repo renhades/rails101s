3616167c6fe6baa4edb66a1543df62a3d8484310
ryan.liu <moomoo9513@gmail.com>
2015-08-29 20:04:27 +0800

    Implement CRUD of group, and add index action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,27 @@
 class GroupsController < ApplicationController
   def index
     flash[:notice] = "早安！你好！"
-    flash[:alert] = "晚安！該睡了！"
-    flash[:warning] = "警告！"
+    # flash[:alert] = "晚安！該睡了！"
+    # flash[:warning] = "警告！"
+
+    @groups = Group.all    
+  end
+
+  def show
+  end
+
+  def new 
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
